f508bcfa13a2d6a7abe90a430e9564fbb1d840b3
ZAHEER-ALI-github-Account <zaheeralibhagar9@gmail.com>
2025-03-29 13:16:53 +0500

    Delete Assignment_0_to_05 directory

--- Assignment_0_to_05/00_intro_python/02_agreement_bot.py
+++
@@ -1,9 +0,0 @@
-def main():
-    # Ask the user for their favorite animal
-    animal = input("What's your favorite animal? ")
-    
-    # Respond with the same favorite animal
-    print(f"My favorite animal is also {animal}!")
-
-if __name__ == '__main__':
-    main()
